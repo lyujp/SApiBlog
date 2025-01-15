841a80a3ef59de5002050953e308eb4439491fee
Lyu, Junpeng <lyujunpeng@outlook.com>
2025-01-15 13:41:28 +0800

    fix: Avoid potential database keyword issues

--- src/main/java/moe/lyu/sapiblog/entity/Setting.java
+++ src/main/java/moe/lyu/sapiblog/entity/Setting.java
@@ -3,23 +3,21 @@ package moe.lyu.sapiblog.entity;
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
-import java.time.ZonedDateTime;
-
 @Data
-@TableName("option")
-public class Option {
+@TableName("setting")
+public class Setting {
     @TableId(type = IdType.AUTO)
     private Integer id;
-    private String key;
-    private String val;
+    private String k;
+    private String v;
     private Boolean optionType;
     @TableField(fill = FieldFill.INSERT)
     private Long createTime;
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateTime;
 
-    public void setKey(String key) {
-        this.key = key.toLowerCase().trim();
+    public void setK(String k) {
+        this.k = k.toLowerCase().trim();
     }
 
     public void setOptionType(Boolean optionType) {
